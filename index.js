9b88ab4708823959887130f739a8fb13f50a6914
AARSHI <aarora0301@gmail.com>
2019-02-03 03:17:32 +0800

    add main index file

---
+++ index.js
@@ -0,0 +1,21 @@
+// start - initialize module aliases
+require('module-alias/register');
+// end - initialize module aliases
+require('./server/api/user/model');
+
+const bootstrapper = require('@core/bootstrapper');
+const shutDownManager = require('@core/shutdownManager');
+const logger = require('@core/logger');
+const config = require('@config');
+const apiRoute = require('@api');
+
+const app = bootstrapper.initiate();
+const port = process.env.PORT || config.DEFAULT_PORT;
+
+// apis are available under /api prefix
+app.use('/api', apiRoute);
+
+const server = app.listen(port, () => {
+  logger.log(`Server started on port ${port}`);
+});
+shutDownManager.manage(server);
